6c30c4eb392b56725a6b2084326078c4362116b8
Oliwia Kamińska <kaminskaoliwia32@gmail.com>
2025-04-16 08:51:30 +0200

    Ścieżka względna SortTestUnitTest, nowe testy

--- SortTestUnitTests/UnitTests_Sort/testsBasicFunctions.cpp
+++ SortTestUnitTests/UnitTests_Sort/testsBasicFunctions.cpp
@@ -1,5 +1,5 @@
 #include "pch.h"
-#include "C:\Users\kamin\Desktop\w³asne_repo\unitTesting\SortTestUnitTests\SortTestUnitTests\Sort.cpp"
+#include "..\SortTestUnitTests\Sort.cpp"
 
 TEST(simpleInsertionTest, test1) {
   int t[] = {5, 6, 3, 2};
@@ -73,6 +73,32 @@ TEST(bubbleSortTest, test2) {
   int expected[] = {-3333333, -5, 0, 0, 0, 0, 6, 7, 8, 9};
 
   bubbleSort(t1, size);
+  for (int i = 1; i < size; i++) {
+    EXPECT_EQ(expected[i], t1[i]);
+  }
+  EXPECT_TRUE(true);
+}
+
+TEST(quickSortTest, test1) {
+  int t[] = {5, 6, 3, 2};
+  int* t1 = t;
+  int size = 4;
+  int expected[] = {2, 3, 5, 6};
+
+  quickSort(t1, size);
+  for (int i = 1; i < size; i++) {
+    EXPECT_EQ(expected[i], t1[i]);
+  }
+  EXPECT_TRUE(true);
+}
+
+TEST(quickSortTest, test2) {
+  int t[] = {9, 8, 7, 6, 0, -5, -3333333, 0, 0, 0};
+  int* t1 = t;
+  int size = 10;
+  int expected[] = {-3333333, -5, 0, 0, 0, 0, 6, 7, 8, 9};
+
+  quickSort(t1, size);
   for (int i = 1; i < size; i++) {
     EXPECT_EQ(expected[i], t1[i]);
   }
